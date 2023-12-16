8ac6cc4b751e5775f817b01b4524b2e033b80bec
Ренета Бонева <reneta.boneva@abv.bg>
2023-12-16 15:26:03 +0200

    Change IRL in UI tests

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-  await page.goto('http://localhost:8080/Add-product');
+  await page.goto('https://renettab02-exam.onrender.com/Add-product');
   const form = await page.$('form');
   expect(form).toBeTruthy();
 });
